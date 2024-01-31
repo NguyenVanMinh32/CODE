91332725e181ab7a3ee6f61d1545dd3a3a5d4369
NguyenVanMinh32 <146708293+NguyenVanMinh32@users.noreply.github.com>
2024-01-31 23:42:34 +0700

    ưer

---
+++ main.js
@@ -0,0 +1,23 @@
+alert('you die')
+
+var r=436/57*37
+console.log(r)
+
+/*setTimeout(function() {
+   prompt('điền tên vào') 
+}, 3000 )
+
+/*setInterval(function() {
+    alert('xin chào bạn')
+},4000)*/
+
+//so sánh bằng: ==
+var x=78; y=5; c=(y/10)**(x/5); d=x%y
+if(x < y) {alert('Right')}
+
+if (x>0 & y<100) {alert('bạn làm đúng')}
+
+alert(c)
+d++;
+alert(d)
+
